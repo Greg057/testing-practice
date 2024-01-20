19b7fdaf5973d7c5b41c1ab5738f8649c35d2496
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-20 11:30:30 +0100

    Add Analyze Array logic & testing

--- js/analyzeArray.js
+++ js/analyzeArray.js
@@ -1,5 +1,16 @@
 function analyzeArray (arr) {
-
+    if (!Array.isArray(arr)) return "Error"
+    if (arr.filter(el => isNaN(el)).length !== 0) return "Error, only numbers allowed"
+    const average = arr.reduce((prev, cur) => prev + cur, 0) / arr.length
+    const min = arr.sort()[0]
+    const max = arr.sort().reverse()[0]
+    const length = arr.length
+    return {
+        average,
+        min,
+        max,
+        length
+      }
 }
 
 module.exports = analyzeArray
--- js/analyzeArray.test.js
+++ js/analyzeArray.test.js
@@ -1,10 +1,32 @@
 const analyzeArray = require("./analyzeArray")
 
 test("properly analyze array", () => {
-    expect(analyzeArray([1,8,3,4,2,6])).toBe({
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual({
         average: 4,
         min: 1,
         max: 8,
         length: 6
       })
+    expect(analyzeArray([-11.5,8,3.5,4,2,6])).toEqual({
+        average: 2,
+        min: -11.5,
+        max: 8,
+        length: 6
+      })
+    expect(analyzeArray([1])).toEqual({
+        average: 1,
+        min: 1,
+        max: 1,
+        length: 1
+      })
+    expect(analyzeArray([0, 0, 0])).toEqual({
+        average: 0,
+        min: 0,
+        max: 0,
+        length: 3
+      })
+    expect(analyzeArray("[1]")).toEqual("Error")
+    expect(analyzeArray("[]")).toEqual("Error")
+    expect(analyzeArray(1,2,3)).toEqual("Error")
+    expect(analyzeArray([1,2,3,"a"])).toEqual("Error, only numbers allowed")
 })
